b234562d272e357512e5993e359584351d04a865
ianlamb77 <ianlamb77@gmail.com>
2016-05-16 16:20:31 -0400

    Add license to source files where applicable

--- src/AppBundle/Controller/RelatedDatasetController.php
+++ src/AppBundle/Controller/RelatedDatasetController.php
@@ -12,6 +12,22 @@ use AppBundle\Entity\Dataset;
 /**
  * A controller that returns the title and slug of a related dataset
  * so it can be displayed in the Twig template
+ *
+ *   This file is part of the Data Catalog project.
+ *   Copyright (C) 2016 NYU Health Sciences Library
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 class RelatedDatasetController extends Controller
 {
