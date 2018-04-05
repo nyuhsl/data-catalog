cb6b2dd05b09ee23e0958751ffb3a41fe76c5171
Joel Marchewka <jpm159@pitt.edu>
2018-04-05 08:58:35 -0400

    New SolrIndexerExamples to reflect deleted/unpublished datasets

--- SolrIndexerExample.php
+++ SolrIndexerExample.php
@@ -1,16 +1,65 @@
 <?php
 
-$output_url = '<BASE URL OF YOUR DATA CATALOG INSTALLATION>/api/dataset/all.json';
-$submit_url = '<BASE URL OF YOUR SOLR INSTALLATION>/solr/data_catalog/update/json?commit=true&overwrite=true';
+$db_output_url = '<BASE URL OF YOUR DATA CATALOG INSTALLATION>/api/dataset/all.json';
+$solr_submit_url = '<BASE URL OF YOUR SOLR INSTALLATION>/solr/data_catalog/update/json?commit=true&overwrite=true';
+$solr_submit_url = '<BASE URL OF YOUR SOLR INSTALLATION>/solr/data_catalog/update/json?commit=true&overwrite=true';
+$solr_remove_url = '<BASE URL OF YOUR SOLR INSTALLATION>/solr/data_catalog/update/?commit=true';
 
-if (! $fh = fopen($output_url, 'r')) {
-    exit("Could not open '{$output_url}'\n");
+if (! $fh = fopen($db_output_url, 'r')) {
+    exit("Could not open '{$db_output_url}'\n");
 }
 
-$json = stream_get_contents($fh);
+$db_json = stream_get_contents($fh);
+$db_json_parsed = json_decode($db_json);
+
 fclose($fh);
 
-foreach (json_decode($json) as $row) {
+#
+# Find items to remove
+
+if (! $fh = fopen($solr_output_url, 'r')) {
+    exit("Could not open '{$solr_output_url}'\n");
+}
+
+$solr_json = stream_get_contents($fh);
+
+fclose($fh);
+
+foreach (json_decode($solr_json)->response->docs as $row) {
+    
+    $found=0;
+    foreach($db_json_parsed as $db_row) {
+        
+        if ($row->id == $db_row->id) {
+            $found=1;
+            break;
+        }
+        
+    }
+    if ($found!=1) {
+        
+        $to_solr = "{'delete': {'id': ".$row->id."}}";
+				$context = stream_context_create([
+								'http' => [
+												'method' => 'POST',
+												'header' => 'Content-Type: application/json' . "\r\n"
+														. 'Content-Length: ' . strlen($to_solr) . "\r\n",
+												'content' => $to_solr,
+										]
+								]);
+
+				file_get_contents($solr_remove_url, null, $context);
+        print "delete ".$row->id."\n";
+        
+    }
+
+}
+
+
+#
+# Find new/added items
+
+foreach ($db_json_parsed as $row) {
     if ($row->dataset_end_date && $row->dataset_start_date) {
         $end_date = $row->dataset_end_date;
         if ('Present' == $row->dataset_end_date) {
@@ -50,5 +99,5 @@ foreach (json_decode($json) as $row) {
                 ]
             ]);
 
-    file_get_contents($submit_url, null, $context);
+    file_get_contents($solr_submit_url, null, $context);
 }
