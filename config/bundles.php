db03098143e0138c6a1d358e61d86d4caf1f2e7b
Ian <ianlamb77@gmail.com>
2020-09-25 14:26:38 -0400

    initial LDAP config and resolving orm-pack errors

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
