faf9083c596f550f0ca59a44661bc5a1955fdb89
Ian <ianlamb77@gmail.com>
2021-06-08 11:12:49 -0400

    Creating popover for Project entity

--- public/js/dataset_details.js
+++ public/js/dataset_details.js
@@ -79,3 +79,29 @@ $(function () {
     }, 200);
   });
 })
+
+
+/**
+* Initialize Project popovers
+*/
+$(function () {
+  $('.dataset-detail-projects [data-toggle="popover"]').popover({
+     'html':true,
+     'animation':false,
+     'trigger':'manual',
+     'placement':'bottom',
+  }).on("mouseenter", function () {
+    var _this = this;
+    $(this).popover("show");
+    $(".popover").on("mouseleave", function () {
+      $(_this).popover('hide');
+    });
+  }).on("mouseleave", function () {
+    var _this = this;
+    setTimeout(function () {
+        if (!$(".popover:hover").length) {
+            $(_this).popover("hide");
+        }
+    }, 200);
+  });
+})
